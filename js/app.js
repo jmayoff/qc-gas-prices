85412dfe2b36d2bfc047230166621db62298a374
jmayoff <jmayoff@gmail.com>
2026-04-02 17:47:32 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,7 @@
-
-console.log("App loaded");
+document.getElementById("load").onclick = async () => {
+  const res = await fetch("PASTE_XLSX_URL_HERE");
+  const buf = await res.arrayBuffer();
+  console.log("Bytes received:", buf.byteLength);
+  document.getElementById("output").textContent =
+    "Downloaded " + buf.byteLength + " bytes";
+};
