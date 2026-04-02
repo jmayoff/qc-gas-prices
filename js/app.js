975ca3a2d0f7435a9eec2e17238d3a6bd3962f76
jmayoff <jmayoff@gmail.com>
2026-04-02 17:48:00 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 document.getElementById("load").onclick = async () => {
-  const res = await fetch("PASTE_XLSX_URL_HERE");
+  const res = await fetch("https://regieessencequebec.ca/data/stations-20260402214504.xlsx");
   const buf = await res.arrayBuffer();
   console.log("Bytes received:", buf.byteLength);
   document.getElementById("output").textContent =
